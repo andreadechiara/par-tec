7bd9e53fbcb33f59abea93bb34152e741644e148
a.dechiara <a.dechiara@NB-ADECHIARA.mainsoft.local>
2017-08-11 17:32:58 +0200

    distribution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,13 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    AppComponent
   ],
   imports: [
     CommonModule
